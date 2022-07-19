7d539a3afe5b336887eb5daca47ec7bb1b31760b
Hanabi <93yutf@gmail.com>
2022-07-19 14:15:43 +0800

    fix sbi.c in lab2 & lab3

--- os/lab3/lib/sbi.c
+++ os/lab3/lib/sbi.c
@@ -74,19 +74,6 @@ struct sbiret sbi_get_impl_version()
     return (struct sbiret){ error, value };
 }
 
-struct sbiret sbi_get_mvendorid()
-{
-    register uint64_t a7 asm("a7") = BASE_EXTENSTION;
-    register uint32_t a6 asm("a6") = 4;
-    register uint64_t error asm("a0");
-    register uint64_t value asm("a1");
-    __asm__ __volatile__("ecall \n\t"
-                 : "=r"(error), "=r"(value)
-                 : "r"(a6), "r"(a7)
-                 : "memory");
-    return (struct sbiret){ error, value };
-}
-
 struct sbiret sbi_probe_extension(long extension_id)
 {
     register uint64_t a7 asm("a7") = BASE_EXTENSTION;
@@ -98,6 +85,18 @@ struct sbiret sbi_probe_extension(long extension_id)
                  : "r"(a6), "r"(a7)
                  : "memory");
     return (struct sbiret){ error, value };
+}
+
+void sbi_shutdown()
+{
+    register uint64_t a0 asm("a0") = 0;
+    register uint64_t a1 asm("a1") = 0;
+    register uint64_t a7 asm("a7") = RESET_EXTENTION;
+    register uint64_t a6 asm("a6") = 0;
+    __asm__ __volatile__("ecall \n\t"
+                 : /* empty output list */
+                 : "r"(a0), "r"(a1), "r"(a6), "r"(a7)
+                 : "memory");
 }
 
 void print_system_infomation()
